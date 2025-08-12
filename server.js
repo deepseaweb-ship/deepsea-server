a1ec49525015698d1befa05b692265773b06c1b9
deepseaweb-ship <atif2akhter@gmail.com>
2025-08-12 13:18:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require(‘express’);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from Deepsea Server!');
+app.get(‘/‘, (req, res) => {
+    res.send(‘Hello from Deepsea Server!’);
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
